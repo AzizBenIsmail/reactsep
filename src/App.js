c4bb1b771c12c90f45d41eec3ef7c3f2c54959a0
AzizBenIsmail <aziz.270700@gmail.com>
2026-09-19 12:22:15 +0100

    s2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import react , {useState} from 'react';
 
 function App() {
+  
+  const name = "Aziz";
+  const age = 30;
+  const isLoggedIn = true;
+  const element = <h1>Bonjour {name}</h1>;
+
+  const user = {
+    name: "Aziz",
+    age: 30,
+  }
+
+  const tableau = ["fawzi", "Ali", "Ahmed"];
+
+  const tableau2 = [
+    { name: "ahmed", age: 30 },
+    { name: "Ali", age: 25 },
+    { name: "Ahmed", age: 20 },
+  ]
+
+  // const count = 0;
+  const [count, setCount] = useState(0);
+  
+  const increment = () => {
+    setCount(count + 1);
+  }
+
+    const deincrement = () => {
+    setCount(count - 1);
+  }
+
+  //const nom = "hello";
+  const [nom, setNom] = useState("hello");
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    {name} <br/>
+    {age} <br/>
+    {element} <br/>
+    {/* {user.name} <br/> */}
+    {tableau[0]} <br/>
+    
+    {tableau.map((item, index) => (
+      <p key={index}>{item}</p>
+    ))}
+
+    {tableau2.map((item, index) => (
+      <p key={index}>{item.name}, {item.age}</p>
+    ))}
+
+
+<br/>
+{count}
+<button onClick={increment}>Increment</button>
+<button onClick={deincrement}>Deincrement</button>
+<br/>
+{nom}
+<input type="text" onChange={(e) => setNom(e.target.value)} />
       </header>
     </div>
   );
---
+++ src/composant/Test.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Test(props) {
+    
+  return (
+    <div>
+      <h1>Bonjour {props.name}</h1>
+    </div>
+  )
+}
